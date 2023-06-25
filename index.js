fb2affae749b328c8db187e3f26e04761d83134e
Priyansu Kumar Nayak <priyansu18.8@gmail.com>
2023-06-25 21:07:20 +0530

    New Update

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 
-function saveLead(){
-    console.log("Clicked")
-}
+let myLeads =[]
+
+const inputBtn=document.getElementById("input-btn")
+const inputEl= document.getElementById("input-el")
+
+
+inputBtn.addEventListener("click",()=>{
+    
+    myLeads.push("www.awesomelead.com")
+    console.log(myLeads)
+})
+
+
+
