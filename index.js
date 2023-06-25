128d36c489aa786e0f9b6751f5e16f180c45e8b6
Priyansu Kumar Nayak <priyansu18.8@gmail.com>
2023-06-25 21:39:45 +0530

    New Update

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 
-let myLeads =[]
+let myLeads =["www.1.com","www.2.com","www.3.com"]
 
 const inputEl= document.getElementById("input-el")
 const inputBtn=document.getElementById("input-btn")
+const ulEl=document.getElementById("ul-el")
 
 
 inputBtn.addEventListener("click",()=>{
     
     myLeads.push(inputEl.value)
     console.log(myLeads)
+    
 })
+
+for(let i=0;i<myLeads.length;i++){
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
+
 
 
 
