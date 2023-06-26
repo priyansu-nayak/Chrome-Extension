e8511e9c96e6a8b18c888d0ddbeb96837d664819
Priyansu Kumar Nayak <priyansu18.8@gmail.com>
2023-06-26 17:51:46 +0530

    New Update

--- index.js
+++ index.js
@@ -4,13 +4,22 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+if(leadsFromLocalStorage){
+    myLeads=leadsFromLocalStorage
+    renderLeads()
+}
+
 inputBtn.addEventListener("click", () => {
   myLeads.push(inputEl.value);
   renderLeads();
   inputEl.value = "";
   
   localStorage.setItem("myLeads",JSON.stringify(myLeads))
-  console.log(localStorage.getItem("myLeads"))
+//   console.log(localStorage.getItem("myLeads"))
+  let leadsFromLocalStorage=JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsFromLocalStorage)
+
 });
 
 function renderLeads() {
